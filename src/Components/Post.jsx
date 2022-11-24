05d566a76827743222a2f829777c1fcc0cda6805
Dharmik Abhangi <abhangidharmik@gmail.com>
2022-11-24 16:19:01 +0530

    Minor Changes

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -4,7 +4,7 @@ import SinglePost from "./SinglePost";
 const Post = () => {
 	return (
 		<div className="w-[100vw] h-[600px] mt-[30px] flex justify-between items-center sxl:px-[15px] md:px-[30px]">
-			<div className="sxl:w-[100%]  md:w-[80%] h-[100%] flex flex-col items-stretch justify-between overflow-y-scroll">
+			<div className="sxl:w-[100%]  md:w-[80%] h-[100%] flex flex-col items-stretch justify-between overflow-y-scroll pr-2 addScrollBar">
 				<SinglePost />
 				<SinglePost />
 				<SinglePost />
